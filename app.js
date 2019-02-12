58993007c3d8a814e00a19bc37438e14f06ba88f
Coochker <khualong@mail.ru>
2019-02-13 02:58:59 +0600

    sssss

--- app.js
+++ app.js
@@ -21,27 +21,6 @@ dsbot.on('ready',function () {
             }
         });
 
-
-    gm('./image.jpg')
-        .flip()
-        .magnify()
-        .rotate('green', 45)
-        .blur(7, 3)
-        .crop(300, 300, 150, 130)
-        .edge(3)
-        .write('./crazy.jpg', function (err) {
-            var embed = new RichEmbed()
-                .setTitle(`rank: ${rows.lvl}ᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠexp: ${rows.exp}/${(nconf.get('startExp') * Math.pow(2, Number(rows.lvl) - 1))}`)
-                .setAuthor(msg.author.username, msg.author.avatarURL)
-
-                .setColor(0x00AE86)
-                .setDescription(setBar(a))
-
-                .setThumbnail("http://i.imgur.com/p2qNFag.png");                                                                        //|
-
-            msg.channel.send(embed);
-        });
-
     console.log(`Logged in as ${dsbot.user.tag}!`);
 });
 dsbot.login(process.env.TOKEN);
