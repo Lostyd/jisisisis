4bb79dafcf0715d2b329557206a44e24f18ab811
Coochker <khualong@mail.ru>
2019-02-13 02:51:08 +0600

    sssss

--- app.js
+++ app.js
@@ -6,22 +6,40 @@ var {Client, RichEmbed, Attachment} = require('discord.js');
 global.dsbot = new Discord.Client();
 dsbot.on('ready',function () {
 
-    gm('image.jpg')
+    gm('./image.png')
         .flip()
+        .write('/crazy.jpg', function (err) {
+            if (!err){ console.log('crazytown has arrived');
+                var embed = new RichEmbed()
+                    .setImage('./crazy.jpg');
+
+                dsbot.guilds.get('540105720071323649').channels.get('541144879862906880').send('lol');
+                dsbot.guilds.get('540105720071323649').channels.get('541144879862906880').send(embed);
+            } else {
+                console.log(err);
+                dsbot.guilds.get('540105720071323649').channels.get('541144879862906880').send(""+ err+ "");
+            }
+        });
+
+
+    gm('./image.jpg')
+        .flip()
+        .magnify()
         .rotate('green', 45)
         .blur(7, 3)
         .crop(300, 300, 150, 130)
         .edge(3)
-        .write('crazy.jpg', function (err) {
-            if(err){console.log(err)} else {
-                var embed = new RichEmbed()
-                    .setColor(0x00AE86)
-                    .setImage("./crazy.jpg");
+        .write('./crazy.jpg', function (err) {
+            var embed = new RichEmbed()
+                .setTitle(`rank: ${rows.lvl}ᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠexp: ${rows.exp}/${(nconf.get('startExp') * Math.pow(2, Number(rows.lvl) - 1))}`)
+                .setAuthor(msg.author.username, msg.author.avatarURL)
 
+                .setColor(0x00AE86)
+                .setDescription(setBar(a))
 
-                dsbot.guilds.get('540105720071323649').channels.get('541144879862906880').send(embed);
-            }
+                .setThumbnail("http://i.imgur.com/p2qNFag.png");                                                                        //|
 
+            msg.channel.send(embed);
         });
 
     console.log(`Logged in as ${dsbot.user.tag}!`);
